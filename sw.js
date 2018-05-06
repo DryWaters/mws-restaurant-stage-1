cdb33c893f3135e8c2df92f035b1ea466b2c0793
Daniel <danwater1@gmail.com>
2018-05-05 18:01:17 -0700

    Fixed the CSS for option selector and map size on mobile

--- sw.js
+++ sw.js
@@ -6,30 +6,30 @@ var urlsToCache = [
   '/img/'
 ];
 
-// setup a cache with the local files already
-// pre-cached.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// // setup a cache with the local files already
+// // pre-cached.
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(cache => {
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
 
-// intercept fetch calls.
-// first check if request is already located in the cache
-// if not, then fetch the event and cache it.
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request).then(response => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+// // intercept fetch calls.
+// // first check if request is already located in the cache
+// // if not, then fetch the event and cache it.
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.open(CACHE_NAME).then(cache => {
+//       return cache.match(event.request).then(response => {
+//         return response || fetch(event.request).then(response => {
+//           cache.put(event.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
